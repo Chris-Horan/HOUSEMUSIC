37d741ae416dbca6543809c329c737735bec29a8
HarryPreet <50694242+HarryPreet@users.noreply.github.com>
2020-07-13 09:10:18 -0700

    Added Home Page

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ userData.loadDatabase();
 
 // Direct node to frontend resources folder (html, css, js)
 app.use(express.static(__dirname + '/public'));
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
