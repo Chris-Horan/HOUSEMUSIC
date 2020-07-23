f885f63cbe3fbbd8387a2d2c47e893892930b134
Derek Chan <allabouthowmylife@gmail.com>
2020-07-22 17:01:15 -0700

    fixed navbar and added dashboard button to admin

--- public/homepage.js
+++ public/homepage.js
@@ -30,3 +30,30 @@ function navbarmanager(){
 		document.getElementById('usernamehere').style.display = 'none';
 	}
 }
+
+function dashboarddisplayUser(){
+	if(sessionStorage.getItem("type") == 'user'){
+	  document.getElementById('usernamehere').innerHTML = sessionStorage.getItem('name');
+	}
+	else if(sessionStorage.getItem("type") == 'admin'){
+	  document.getElementById('usernamehere').innerHTML = sessionStorage.getItem('name');
+	}
+	dashboardnavbarmanager();
+}
+
+function dashboardnavbarmanager(){
+	if(sessionStorage.getItem("name") == 'admin'){
+		document.getElementById('usernamehere').style.display = 'inline';
+		document.getElementById('adminpaneltag').style.display = 'inline';
+	}
+	else if(sessionStorage.getItem("name") != null && sessionStorage.getItem("name") != 'null' ){
+		document.getElementById('usernamehere').style.display = 'inline';
+		document.getElementById('adminpaneltag').style.display = 'none';
+	}
+	else{
+	/*This should never reach here, but just in case it does, i kept it here */
+		document.getElementById('adminpaneltag').style.display = 'none';
+		document.getElementById('dashboardtag').style.display = 'none';
+		document.getElementById('usernamehere').style.display = 'none';
+	}
+}
